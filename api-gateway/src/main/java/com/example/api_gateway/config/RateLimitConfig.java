b50460a055c3f2f4d87303422b2c9189ccf02f46
Franco Barrera <franco.barrera@alenia.io>
2025-11-06 11:34:54 -0300

    Added config files

---
+++ api-gateway/src/main/java/com/example/api_gateway/config/RateLimitConfig.java
@@ -0,0 +1,17 @@
+package com.example.apigateway;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange ->
+                Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    }
+}
